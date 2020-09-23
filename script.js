e3fff65e4ab68295b3c80b7ac88ffa13e5a35f0f
Brandon Tripp <brandongtripp@gmail.com>
2020-09-23 14:33:13 -0600

    added closing capability for modal and correct url validation

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-console.log('Hello');
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+// Show Modal, Focus on Inpute
+
+function showModal() {
+   modal.classList.add('show-modal');
+   websiteNameEl.focus();
+}
+
+// Validate Form
+
+function validate(nameValue, urlValue) {
+   const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+   const regex = new RegExp(expression);
+   if (!nameValue || !urlValue) {
+      alert('Please submit values for both fields. ');
+      return false;
+   }
+   if (!urlValue.match(regex)) {
+      alert('Please provide a valid web address');
+      return false;
+   }
+   // valid
+   return true;
+}
+
+// Modal Event Listener
+
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false );
+
+
+// Handle data from form 
+
+function storeBookmark(e) {
+   e.preventDefault();
+   const nameValue = websiteNameEl.value;
+   let urlValue = websiteUrlEl.value;
+   if(!urlValue.includes('http://', 'https://')) {
+      urlValue = `https://${urlValue}`;
+   }
+   console.log(nameValue, urlValue);
+   if (!validate(nameValue, urlValue)) {
+      return false;
+   }
+   
+}
+// Event Listener
+
+bookmarkForm.addEventListener('submit', storeBookmark)
