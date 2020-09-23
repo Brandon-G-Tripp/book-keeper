9177a9f4aa2129555e38efcc9cb56beaa12b712a
Brandon Tripp <brandongtripp@gmail.com>
2020-09-23 16:24:09 -0600

    added final touches to deleting bookmarks and removing them from localStorage

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
+
 // Show Modal, Focus on Inpute
 
 function showModal() {
@@ -36,6 +39,73 @@ modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => e.target === modal ? modal.classList.remove('show-modal') : false );
 
+// Build Bookmarks DOM
+
+function buildBookmarks() {
+   // Remove all bookmark elements
+   bookmarksContainer.textContent = '';
+   // Build Items
+   bookmarks.forEach((bookmark) => {
+      const { name, url } = bookmark;
+      // Item
+      const item = document.createElement('div');
+      item.classList.add('item');
+      // Close Icon
+      const closeIcon = document.createElement('i');
+      closeIcon.classList.add('far', 'fa-trash-alt');
+      closeIcon.setAttribute('title', 'Delete Bookmark');
+      closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+      // Favicon / Link Container
+      const linkInfo = document.createElement('div');
+      linkInfo.classList.add('name');
+      // Favicon 
+      const favicon = document.createElement('img');
+      favicon.setAttribute('src', `http://www.google.com/s2/favicons?domain=${url}`);
+      favicon.setAttribute('alt', 'Favicon');
+      // Link
+      const link = document.createElement('a');
+      link.setAttribute('href', `${url}`);
+      link.setAttribute('target', '_blank');
+      link.textContent = name;
+      // Append to bookmarks container
+      linkInfo.append(favicon, link);
+      item.append(closeIcon, linkInfo);
+      bookmarksContainer.appendChild(item);
+   });
+}
+
+
+// Fetch bookmarks
+
+function fetchBookmarks() {
+   // Get bookmarks from localstorage if available
+   if (localStorage.getItem('bookmarks')) {
+      bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+   } else {
+      // Create bookmarks array in local storage
+      bookmarks = [
+         {
+            name: 'Google',
+            url: 'https://google.com',
+         },
+      ];
+      localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+   }
+   buildBookmarks();
+}
+
+// Delete Bookmark
+
+function deleteBookmark(url) {
+   bookmarks.forEach((bookmark, i) => {
+      if (bookmark.url === url) {
+         bookmarks.splice(i, 1);
+      }
+   });
+   //Update bookmarks array in localStorage, repopulate DOM
+   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+   fetchBookmarks();
+}
 
 // Handle data from form 
 
@@ -46,12 +116,24 @@ function storeBookmark(e) {
    if(!urlValue.includes('http://', 'https://')) {
       urlValue = `https://${urlValue}`;
    }
-   console.log(nameValue, urlValue);
    if (!validate(nameValue, urlValue)) {
       return false;
    }
-   
+   const bookmark = {
+      name: nameValue,
+      url: urlValue,
+   };
+   bookmarks.push(bookmark);
+   console.log(bookmarks);
+   localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+   fetchBookmarks();
+   bookmarkForm.reset();
+   websiteNameEl.focus();
 }
 // Event Listener
 
-bookmarkForm.addEventListener('submit', storeBookmark)
+bookmarkForm.addEventListener('submit', storeBookmark);
+
+// On load, fetch bookmarks
+
+fetchBookmarks();
